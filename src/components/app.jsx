0d1502c3a5c24e73b931fd4868ce0d70c8e751a8
PSL <pramodsankar.l@gmail.com>
2015-12-13 21:16:39 -0500

    * Integrate Material UI

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import MessageList from './MessageList.jsx';
+import mui from 'material-ui';
+
+let ThemeManager = new mui.Styles.ThemeManager();
+let Colors = mui.Styles.Colors;
+let AppBar = mui.AppBar;
 
 class App extends React.Component{
     constructor(){
         super();
+        ThemeManager.setPalette({
+            primary1Color:Colors.blue500,
+            primary2Color:Colors.blue700,
+            primary3Color:Colors.blue100,
+            accent1Color:Colors.pink400
+        });
+    }
+
+    static childContextTypes = {
+        muiTheme: React.PropTypes.object
+    };
+
+    getChildContext(){
+        let muiTheme = ThemeManager.getCurrentTheme();
+
+        return {
+            muiTheme
+        };
     }
 
     render(){
         return (
-            <MessageList />
+            <div>
+                <AppBar title="Chat Hub"/>
+                <MessageList />
+            </div>
         )
     }
 }
